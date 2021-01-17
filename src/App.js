f8771c3bf75444042ea9fae63b05152b2131d951
Kala Foods <guhan.delta@gmail.com>
2021-01-17 10:07:52 -0500

    Added the particle animaiton to the landing page, using react-particles-js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Header, Navbar } from './components'
+import Particles from 'react-particles-js'
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -6,6 +7,26 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <>
+      <Particles
+        params={{
+          particles:{
+            number:{
+              value: 30,
+              density:{
+                enable: true,
+                value_area: 900
+              }
+            },
+            shape:{
+              type: 'circle',
+              stroke: {
+                width: 6,
+                color: '#f9ab00'
+              }
+            }
+          }
+        }}
+      />
       <Navbar />  
       <Header />
     </>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,22 @@
-import React from 'react'
+import Typed from 'react-typed'
 
 const Header = () => {
     return (
         <div className="header-wrapper">
             <div className="main-info">
-                <h1>UI and Fullstack Developer</h1>
+                <h1 className="title">UI & Fullstack Developer</h1>
+                <Typed 
+                    className="typed-text"
+                    strings={["UI Designer", "Fullstack Developer", "Blockchain Enthusiast", "Dog Lover"]}
+                    typeSpeed={40}
+                    backSpeed={50}
+                    loop
+                />
+                <a href="#contact" className="btn-main-offer">Contact Me</a>
             </div>
         </div>
     )
 }
 
 export default Header
+
