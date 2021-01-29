1f65853033e947b84b4c7a8fdf8537378725f6a8
Kala Foods <guhan.delta@gmail.com>
2021-01-29 06:16:45 -0500

    Built the basic Component structure for teh experience timeline

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -4,7 +4,65 @@ const Experience = () => {
     return (
         <div className="experience">
             <div className="d-flex justify-content-center my-5">
-                {/* <h3>My Experience</h3> */}
+                <h3>My Experience</h3>
+            </div>
+            <div className="container experience-wrapper">
+                <div className="timeline-block timeline-block-right">
+                    <div className="marker"></div>
+                    <div className="timeline-content">
+                        <h3>2012 - 2015</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere, ipsam! Tempore provident eius fugit autem sint. Officiis ut tempora facilis accusantium officia voluptatem explicabo temporibus incidunt, ratione, nisi molestiae porro?</p>
+                    </div>
+                </div>
+                <div className="timeline-block timeline-block-left">
+                    <div className="marker"></div>
+                    <div className="timeline-content"> 
+                        <h3>2012 - 2015</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere, ipsam! Tempore provident eius fugit autem sint. Officiis ut tempora facilis accusantium officia voluptatem explicabo temporibus incidunt, ratione, nisi molestiae porro?</p>
+                    </div>
+                </div>
+                <div className="timeline-block timeline-block-right">
+                    <div className="marker"></div>
+                    <div className="timeline-content">
+                        <h3>2012 - 2015</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere, ipsam! Tempore provident eius fugit autem sint. Officiis ut tempora facilis accusantium officia voluptatem explicabo temporibus incidunt, ratione, nisi molestiae porro?</p>
+                    </div>
+                </div>
+                <div className="timeline-block timeline-block-left">
+                    <div className="marker"></div>
+                    <div className="timeline-content">
+                        <h3>2012 - 2015</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere, ipsam! Tempore provident eius fugit autem sint. Officiis ut tempora facilis accusantium officia voluptatem explicabo temporibus incidunt, ratione, nisi molestiae porro?</p>
+                    </div>
+                </div>
+                <div className="timeline-block timeline-block-right">
+                    <div className="marker"></div>
+                    <div className="timeline-content">
+                        <h3>2012 - 2015</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere, ipsam! Tempore provident eius fugit autem sint. Officiis ut tempora facilis accusantium officia voluptatem explicabo temporibus incidunt, ratione, nisi molestiae porro?</p>
+                    </div>
+                </div>
+                <div className="timeline-block timeline-block-left">
+                    <div className="marker"></div>
+                    <div className="timeline-content">
+                        <h3>2012 - 2015</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere, ipsam! Tempore provident eius fugit autem sint. Officiis ut tempora facilis accusantium officia voluptatem explicabo temporibus incidunt, ratione, nisi molestiae porro?</p>
+                    </div>
+                </div>
+                <div className="timeline-block timeline-block-right">
+                    <div className="marker"></div>
+                    <div className="timeline-content">
+                        <h3>2012 - 2015</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere, ipsam! Tempore provident eius fugit autem sint. Officiis ut tempora facilis accusantium officia voluptatem explicabo temporibus incidunt, ratione, nisi molestiae porro?</p>
+                    </div>
+                </div>
+                <div className="timeline-block timeline-block-left">
+                    <div className="marker"></div>
+                    <div className="timeline-content">
+                        <h3>2012 - 2015</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere, ipsam! Tempore provident eius fugit autem sint. Officiis ut tempora facilis accusantium officia voluptatem explicabo temporibus incidunt, ratione, nisi molestiae porro?</p>
+                    </div>
+                </div>
             </div>
         </div>
     )
