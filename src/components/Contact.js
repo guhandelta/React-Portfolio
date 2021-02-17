6f7d83a6484c9c386f0697a217698f9d1e48c0da
Kala Foods <guhan.delta@gmail.com>
2021-02-17 17:45:39 -0500

    Provided some interactive line animations for the input fields

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -11,14 +11,29 @@ const Contact = () => {
                 <div className="container">
                     <div className="row">
                         <div className="col-md-6 col-xs-12">
-                            <input id="name" type="text" className="form-control details" placeholder="Name" name="name" />
-                            <input id="phone" type="text" className="form-control details" placeholder="Phone Number" name="phone" />
-                            <input id="email" type="text" className="form-control details" placeholder="Email" name="email" />
-                            <input id="subject" type="text" className="form-control details" placeholder="Subject" name="subject" />
+                                <div className="text-center">
+                                <input id="name" type="text" className="form-control details" placeholder="Name" name="name" />
+                            <div className="line"></div>
+                            </div>
+                            <div className="text-center">
+                                <input id="phone" type="text" className="form-control details" placeholder="Phone Number" name="phone" />
+                                <div className="line"></div>
+                            </div>
+                                <div className="text-center">
+                                <input id="email" type="text" className="form-control details" placeholder="Email" name="email" />
+                            <div className="line"></div>
+                            </div>
+                            <div className="text-center">
+                                <input id="subject" type="text" className="form-control details" placeholder="Subject" name="subject" />
+                                <div className="line"></div>
+                            </div>
                         </div>
                         <div className="col-md-6 col-xs-12">
-                            <textarea id="description" className="form-control" placeholder="Description" type="text" name="description"></textarea>
-                            <button className="btn-main-offer contact-btn" type="submit">Contact Me</button>
+                            <div className="text-center">
+                                <textarea id="description" className="form-control" placeholder="Description" type="text" name="description"></textarea>
+                                <div className="line"></div>
+                                <button className="btn-main-offer contact-btn" type="submit">Contact Me</button>
+                            </div>
                         </div>
                     </div>
                 </div>
